e2510943ff192450059b1c3614d35295320ae8db
João Alfredo Pires <joaoalfredopires.prof@gmail.com>
2021-10-16 17:29:29 -0300

    Allure funcionando corretamente

--- src/test/java/elementMapper/ProductDetailPageElementMapper.java
+++ src/test/java/elementMapper/ProductDetailPageElementMapper.java
@@ -3,8 +3,6 @@ package elementMapper;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import javax.xml.ws.wsaddressing.W3CEndpointReference;
-
 public class ProductDetailPageElementMapper {
 
     @FindBy(css = "h1[itemprop=name]")
